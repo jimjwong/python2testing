e0a5e329c2a291c244c81c9267796bdefb8ca2e6
Jim J. Wong <39659900+jimjwong@users.noreply.github.com>
2023-07-19 16:32:29 +0800

    new car library and program added by jim

---
+++ car.py
@@ -0,0 +1,27 @@
+class Car:
+#     pass #as a placeholder
+
+#     make = None
+#     model = None
+#     year = None
+#     color = None
+    
+##### These are the attributes of the object, describing the object #####
+    def __init__(self, make, model, year, color): #use to construct objects for us
+#         self.make = None
+#         self.model = None
+#         self.year = None
+#         self.color = None
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+        
+##### There are the methods related to the object, describing what it can do #####
+    def drive(self): #self refers to the attribute using this function
+#         print("This car is driving.")
+        print(f"This {self.model} is driving")
+    
+    def stop(self):
+#         print("This car is stopped")
+        print(f"This {self.model} is stopped")
